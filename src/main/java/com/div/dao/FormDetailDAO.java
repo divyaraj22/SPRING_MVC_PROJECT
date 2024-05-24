7efe70b173de1066b5c344bbaa1420d395188827
divyaraj22 <20it030@charusat.edu.in>
2024-05-24 22:12:15 +0530

    updated dao layer

--- src/main/java/com/div/dao/FormDetailDAO.java
+++ src/main/java/com/div/dao/FormDetailDAO.java
@@ -9,8 +9,8 @@ import javax.persistence.criteria.Order;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import org.hibernate.SessionFactory;
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Repository;
 import com.div.pojo.FormDetail;
 import com.div.pojo.SearchCriteria;
@@ -21,93 +21,92 @@ import com.div.dto.UserDTO;
 @Repository
 public class FormDetailDAO extends GenericDAO<FormDetail, Integer> {
 
-	private HibernateTemplate hibernateTemplate;
-	private SessionFactory sessionFactory;
+    private SessionFactory sessionFactory;
 
-	@Autowired
-	public FormDetailDAO(HibernateTemplate hibernateTemplate, SessionFactory sessionFactory) {
-		super(FormDetail.class);
-		this.hibernateTemplate = hibernateTemplate;
-		this.sessionFactory = sessionFactory;
-	}
+    @Autowired
+    public FormDetailDAO(SessionFactory sessionFactory) {
+        super(FormDetail.class);
+        this.sessionFactory = sessionFactory;
+    }
 
-	public List<FormDetailDTO> findByUser(UserDTO userDto) {
-		User user = userDto.toModel();
-		List<FormDetail> formDetails = hibernateTemplate.execute(session -> {
-			CriteriaBuilder cb = session.getCriteriaBuilder();
-			CriteriaQuery<FormDetail> cq = cb.createQuery(FormDetail.class);
-			Root<FormDetail> root = cq.from(FormDetail.class);
-			cq.select(root).where(cb.equal(root.get("user"), user));
-			return session.createQuery(cq).getResultList();
-		});
-		return formDetails.stream().map(FormDetailDTO::fromModel).collect(Collectors.toList());
-	}
+    public List<FormDetailDTO> findByUser(UserDTO userDto) {
+        User user = userDto.toModel();
+        Session session = sessionFactory.getCurrentSession();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<FormDetail> cq = cb.createQuery(FormDetail.class);
+        Root<FormDetail> root = cq.from(FormDetail.class);
+        cq.select(root).where(cb.equal(root.get("user"), user));
+        List<FormDetail> formDetails = session.createQuery(cq).getResultList();
+        return formDetails.stream().map(FormDetailDTO::fromModel).collect(Collectors.toList());
+    }
 
-	public List<FormDetailDTO> findFreeAccessWithExpiry(Date today) {
-		List<FormDetail> formDetails = hibernateTemplate.execute(session -> {
-			CriteriaBuilder cb = session.getCriteriaBuilder();
-			CriteriaQuery<FormDetail> cq = cb.createQuery(FormDetail.class);
-			Root<FormDetail> root = cq.from(FormDetail.class);
-			cq.select(root).where(
-					cb.and(cb.equal(root.get("accessCategory"), "free"), cb.equal(root.get("freeViewExpiry"), today)));
-			return session.createQuery(cq).getResultList();
-		});
-		return formDetails.stream().map(FormDetailDTO::fromModel).collect(Collectors.toList());
-	}
+    public List<FormDetailDTO> findFreeAccessWithExpiry(Date today) {
+        Session session = sessionFactory.getCurrentSession();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<FormDetail> cq = cb.createQuery(FormDetail.class);
+        Root<FormDetail> root = cq.from(FormDetail.class);
+        cq.select(root).where(
+                cb.and(cb.equal(root.get("accessCategory"), "free"), cb.equal(root.get("freeViewExpiry"), today)));
+        List<FormDetail> formDetails = session.createQuery(cq).getResultList();
+        return formDetails.stream().map(FormDetailDTO::fromModel).collect(Collectors.toList());
+    }
 
-	public List<FormDetailDTO> getSortedFormDetails(String sortField, String sortOrder) {
-		List<FormDetail> formDetails = hibernateTemplate.execute(session -> {
-			CriteriaBuilder cb = session.getCriteriaBuilder();
-			CriteriaQuery<FormDetail> cq = cb.createQuery(FormDetail.class);
-			Root<FormDetail> root = cq.from(FormDetail.class);
-			Order order = "asc".equalsIgnoreCase(sortOrder) ? cb.asc(root.get(sortField))
-					: cb.desc(root.get(sortField));
-			cq.select(root).orderBy(order);
-			return session.createQuery(cq).getResultList();
-		});
-		return formDetails.stream().map(FormDetailDTO::fromModel).collect(Collectors.toList());
-	}
+    public List<FormDetailDTO> getSortedFormDetails(String sortField, String sortOrder) {
+        Session session = sessionFactory.getCurrentSession();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<FormDetail> cq = cb.createQuery(FormDetail.class);
+        Root<FormDetail> root = cq.from(FormDetail.class);
+        Order order = "asc".equalsIgnoreCase(sortOrder) ? cb.asc(root.get(sortField))
+                : cb.desc(root.get(sortField));
+        cq.select(root).orderBy(order);
+        List<FormDetail> formDetails = session.createQuery(cq).getResultList();
+        return formDetails.stream().map(FormDetailDTO::fromModel).collect(Collectors.toList());
+    }
 
-	public void save(FormDetailDTO formDetailDto) {
-		save(formDetailDto.toModel());
-	}
+    public void save(FormDetailDTO formDetailDto) {
+        sessionFactory.getCurrentSession().persist(formDetailDto.toModel());
+    }
 
-	public void update(FormDetailDTO formDetailDto) {
-		update(formDetailDto.toModel());
-	}
+    public void update(FormDetailDTO formDetailDto) {
+        sessionFactory.getCurrentSession().merge(formDetailDto.toModel());
+    }
 
-	public FormDetailDTO findOneDTO(Integer id) {
-		FormDetail formDetail = findOne(id);
-		return FormDetailDTO.fromModel(formDetail);
-	}
+    public FormDetailDTO findOneDTO(Integer id) {
+        FormDetail formDetail = sessionFactory.getCurrentSession().get(FormDetail.class, id);
+        return FormDetailDTO.fromModel(formDetail);
+    }
 
-	public void deleteByIdDTO(Integer id) {
-		deleteById(id);
-	}
+    public void deleteByIdDTO(Integer id) {
+        Session session = sessionFactory.getCurrentSession();
+        FormDetail formDetail = session.get(FormDetail.class, id);
+        if (formDetail != null) {
+            session.delete(formDetail);
+        }
+    }
 
-	public List<FormDetailDTO> findByUserWithCriteria(UserDTO userDto, SearchCriteria searchCriteria) {
-		User user = userDto.toModel();
-		return hibernateTemplate.execute(session -> {
-			CriteriaBuilder cb = session.getCriteriaBuilder();
-			CriteriaQuery<FormDetail> cq = cb.createQuery(FormDetail.class);
-			Root<FormDetail> root = cq.from(FormDetail.class);
+    public List<FormDetailDTO> findByUserWithCriteria(UserDTO userDto, SearchCriteria searchCriteria) {
+        User user = userDto.toModel();
+        Session session = sessionFactory.getCurrentSession();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<FormDetail> cq = cb.createQuery(FormDetail.class);
+        Root<FormDetail> root = cq.from(FormDetail.class);
 
-			Predicate predicate = cb.equal(root.get("user"), user);
+        Predicate predicate = cb.equal(root.get("user"), user);
 
-			if (searchCriteria.getSearchTitle() != null && !searchCriteria.getSearchTitle().isEmpty()) {
-				predicate = cb.and(predicate, cb.like(root.get("title"), "%" + searchCriteria.getSearchTitle() + "%"));
-			}
+        if (searchCriteria.getSearchTitle() != null && !searchCriteria.getSearchTitle().isEmpty()) {
+            predicate = cb.and(predicate, cb.like(root.get("title"), "%" + searchCriteria.getSearchTitle() + "%"));
+        }
 
-			cq.where(predicate);
+        cq.where(predicate);
 
-			if (searchCriteria.getSortField() != null && searchCriteria.getSortOrder() != null) {
-				Order order = "asc".equalsIgnoreCase(searchCriteria.getSortOrder())
-						? cb.asc(root.get(searchCriteria.getSortField()))
-						: cb.desc(root.get(searchCriteria.getSortField()));
-				cq.orderBy(order);
-			}
+        if (searchCriteria.getSortField() != null && searchCriteria.getSortOrder() != null) {
+            Order order = "asc".equalsIgnoreCase(searchCriteria.getSortOrder())
+                    ? cb.asc(root.get(searchCriteria.getSortField()))
+                    : cb.desc(root.get(searchCriteria.getSortField()));
+            cq.orderBy(order);
+        }
 
-			return session.createQuery(cq).getResultList();
-		}).stream().map(FormDetailDTO::fromModel).collect(Collectors.toList());
-	}
+        List<FormDetail> formDetails = session.createQuery(cq).getResultList();
+        return formDetails.stream().map(FormDetailDTO::fromModel).collect(Collectors.toList());
+    }
 }
--- src/main/webapp/WEB-INF/views/ViewAllDetails.jsp
+++ src/main/webapp/WEB-INF/views/ViewAllDetails.jsp
@@ -1,55 +1,127 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<%@page isELIgnored="false"%>
 <%@ page import="com.div.util.ImageUtil"%>
-<%@ page isELIgnored="false"%>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>View All Details</title>
+<title>View All Details</title>
+<style>
+.dropdown {
+	position: relative;
+	display: inline-block;
+}
+
+.dropdown-content {
+	display: none;
+	position: absolute;
+	background-color: #f9f9f9;
+	min-width: 160px;
+	box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+	z-index: 1;
+}
+
+.dropdown-content a {
+	color: black;
+	padding: 12px 16px;
+	text-decoration: none;
+	display: block;
+}
+
+.dropdown-content a:hover {
+	background-color: #f1f1f1
+}
+
+.dropdown:hover .dropdown-content {
+	display: block;
+}
+
+.dropdown:hover .dropbtn {
+	background-color: #334FFF
+}
+</style>
 </head>
 <body>
-    <h2>View All Details</h2>
+	<h2>View All Details</h2>
 
-    <table border="1">
-        <thead>
-            <tr>
-                <th>Title</th>
-                <th>Public URL</th>
-                <th>Access Category</th>
-                <th>Published Date</th>
-                <th>Banner</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            <c:forEach var="detail" items="${details}">
-                <tr>
-                    <td>${detail.title}</td>
-                    <td>${detail.publicURL}</td>
-                    <td>${detail.accessCategory}</td>
-                    <td><fmt:formatDate value="${detail.publishedDate}" pattern="yyyy-MM-dd" /></td>
-                    <td>
-                        <c:if test="${not empty detail.banner}">
-                            <img src="${ImageUtil.getBase64EncodedImage(detail.banner, detail.contentType)}" width="120" height="100" />
-                        </c:if>
-                    </td>
-                    <td>
-                        <a href="deleteDetail?id=${detail.id}">Delete</a>
-                        <a href="editDetail?id=${detail.id}">Edit</a>
-                    </td>
-                </tr>
-            </c:forEach>
-        </tbody>
-    </table>
+	<form action="viewAll" method="get">
+		<label for="searchTitle">Search by Title:</label> <input type="text"
+			id="searchTitle" name="searchTitle"
+			value="${searchCriteria.searchTitle}"> <input type="hidden"
+			name="sortField" value="${searchCriteria.sortField}"> <input
+			type="hidden" name="sortOrder" value="${searchCriteria.sortOrder}">
+		<input type="submit" value="Search">
+	</form>
+
+	<table border="1">
+		<thead>
+			<tr>
+				<th>Title
+					<div class="dropdown">
+						<button class="dropbtn">Sort</button>
+						<div class="dropdown-content">
+							<a
+								href="viewAll?searchTitle=${searchCriteria.searchTitle}&sortField=title&sortOrder=asc">Ascending</a>
+							<a
+								href="viewAll?searchTitle=${searchCriteria.searchTitle}&sortField=title&sortOrder=desc">Descending</a>
+						</div>
+					</div>
+				</th>
+				<th>Public URL
+					<div class="dropdown">
+						<button class="dropbtn">Sort</button>
+						<div class="dropdown-content">
+							<a
+								href="viewAll?searchTitle=${searchCriteria.searchTitle}&sortField=publicURL&sortOrder=asc">Ascending</a>
+							<a
+								href="viewAll?searchTitle=${searchCriteria.searchTitle}&sortField=publicURL&sortOrder=desc">Descending</a>
+						</div>
+					</div>
+				</th>
+				<th>Access Category
+					<div class="dropdown">
+						<button class="dropbtn">Sort</button>
+						<div class="dropdown-content">
+							<a
+								href="viewAll?searchTitle=${searchCriteria.searchTitle}&sortField=accessCategory&sortOrder=asc">Ascending</a>
+							<a
+								href="viewAll?searchTitle=${searchCriteria.searchTitle}&sortField=accessCategory&sortOrder=desc">Descending</a>
+						</div>
+					</div>
+				</th>
+				<th>Published Date</th>
+				<th>Banner</th>
+				<th>Action</th>
+			</tr>
+		</thead>
+		<tbody>
+			<c:forEach var="detail" items="${details}">
+				<tr>
+					<td>${detail.title}</td>
+					<td>${detail.publicURL}</td>
+					<td>${detail.accessCategory}</td>
+					<td><fmt:formatDate value="${detail.publishedDate}"
+							pattern="yyyy-MM-dd" /></td>
+					<td><c:if test="${not empty detail.banner}">
+							<img
+								src="${ImageUtil.getBase64EncodedImage(detail.banner, detail.contentType)}"
+								width="120" height="100" />
+						</c:if></td>
+					<td><a href="deleteDetail?id=${detail.id}" class="btn-delete">Delete</a>
+						<a href="editDetail?id=${detail.id}" class="btn-edit">Edit</a></td>
+				</tr>
+			</c:forEach>
+		</tbody>
+	</table>
+
 	<form action="signup" method="get"
 		style="text-align: center; margin-top: 20px;">
 		<input type="submit" value="Home" />
 	</form>
-		<form action="addDetails" method="get"
+	<form action="addDetails" method="get"
 		style="text-align: center; margin-top: 20px;">
-		<input type="submit" value="Add Form Details" />
+		<input type="submit" value="Go To Form">
 	</form>
 </body>
 </html>
